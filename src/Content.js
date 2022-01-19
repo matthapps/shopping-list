a04fbabb1fe5cdd023984225acfe57f8983c216b
matthapps <mhheinrichs@gmail.com>
2022-01-19 15:28:46 -0800

    first commit

---
+++ src/Content.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { FaTrashAlt } from 'react-icons/fa'
+
+const Content = () => {
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: false,
+            item: "One half pound bag of chocolate almonds"
+        },
+    
+        {
+            id: 2,
+            checked: false,
+            item: "One half pound bag of chocolate almonds"
+        },
+
+        {
+            id: 3,
+            checked: false,
+            item: "One half pound bag of chocolate almonds"
+        }
+    ]);
+
+    return (
+        <main>
+            <ul>
+                {items.map((item) => (
+                    <li className="item" key={item.id}>
+                        <input
+                            type="checkbox"
+                            checked={item.checked}
+                        />
+                        <label htmlFor="">{item.item}</label>
+                        <FaTrashAlt 
+                            role="button" 
+                            tabIndex='0' 
+                        />
+                    </li>
+                ))}
+            </ul>
+        </main>
+    )
+}
+
+export default Content;
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+
+    let today = new Date();
+    return (
+        <footer>
+            <p>Copyright &copy; {today.getFullYear()}</p>
+        </footer>
+    )
+}
+
+export default Footer;
